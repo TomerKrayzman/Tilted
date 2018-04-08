7248f43f4ce45a6053e9f106f1960daa7269a283
saeberg <mangreen20@gmail.com>
2018-04-08 03:45:58 -0400

    add magic algorithm

---
+++ tiltulatorStarter/Bootstrap-4-templates-master/one-column-listing/js/Algo.js
@@ -0,0 +1,41 @@
+var wins, losses, curStreak, relativePerformance, matchHistory;
+
+export function tiltDemo(matchList, summId) {
+    winsList = parseMatches(matchList, summId)
+    sumWinrate = winrate(winsList);
+    sumStreak = streak(winsList);
+    
+}
+
+function winrate(winsList) {
+    wins = 0; losses = 0;
+    for (result in winsList) {
+        if (result = true)
+            wins++;
+        else
+            losses++;
+    }
+    return (wins/losses);
+}
+
+function sumStreak(winsList) {
+    lastGameResult = winsList[winsList.length() - 1];
+    count = 1;
+    i = winsList.length() - 2;
+    while(i >= 0 && winsList[i] === lastGameResult) {
+        cpunt++;
+    }
+    return lastGameResult ? count : -count;
+}
+
+function parseMatches(matchList, summId) { 
+    return matchList.map(match => parseMatchWin(match, summId));
+}
+
+function parseMatchWin(match, summId) { 
+    for each(player in match.participants) {
+        if (player.participantId === summId) 
+            return player.stats.win
+    }
+    return false; // should never happen
+}
