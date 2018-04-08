a69e9aa69880762b7d3c3053ec89fa1ae9467e0e
Braeden Mollot <3mollot@gmail.com>
2018-04-08 01:08:37 -0400

    Fix jsquery path

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 const apiKey = "RGAPI-09105804-5cbb-49dc-9071-83d9d7c15779"
 
 const apiBase = "https://loltilted.com/api"
-const summonerByNameUri = (sname) => `${apiBase}/lol/summoner/v3/summoners/by-name/${sname}?${apiKey}`
-const summonerById = (id) => `${apiBase}/lol/summoner/v3/summoners/${id}?${apiKey}`
+const summonerByNameUri = (sname) => `${apiBase}/lol/summoner/v3/summoners/by-name/${sname}`
+const summonerById = (id) => `${apiBase}/lol/summoner/v3/summoners/${id}`
 
 document.querySelector('#sname-but').addEventListener('click', () => {
   const snameInput = document.querySelector('#sname-input')
@@ -14,7 +14,7 @@ document.querySelector('#sname-but').addEventListener('click', () => {
   xhr.open('GET', summonerByNameUri(sname), false) // syncronous request
   // xhr.setRequestHeader("Origin", null)
   // xhr.setRequestHeader("Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8")
-  // xhr.setRequestHeader("X-Riot-Token", "RGAPI-09105804-5cbb-49dc-9071-83d9d7c15779")
+  xhr.setRequestHeader("X-Riot-Token", "RGAPI-09105804-5cbb-49dc-9071-83d9d7c15779")
   // xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5")
   // xhr.setRequestHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0")
   xhr.send()
