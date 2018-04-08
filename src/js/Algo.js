a6d5129eec5a9e2ddaa27b0b3818bc8faa926cb8
saeberg <mangreen20@gmail.com>
2018-04-08 07:28:10 -0400

    algo?

--- src/js/Algo.js
+++ src/js/Algo.js
@@ -4,11 +4,46 @@ function tiltDemo(matchList, summId) {
    // winsList = parseMatches(matchList, summId)
     //sumWinrate = winrate(winsList);
     //sumStreak = streak(winsList);
-    return [69];
+    return [999];
 }
 
 function totalTilt(matchList, summId) {
-    var tiltval
+    var overallTilt = 0;
+    var tiltPerGame = matchList.map(match => gameTilt(match, summId));
+    for (var i = 0; i < matchList.length; i++) { 
+        overallTilt += tiltPerGame[i] * (matchList.length - i));
+    }
+    return [tiltPerGame, overallTilt/matchList.length];
+}
+
+function gameTilt(match, summId) {
+    for (var playerId of match.participantIdentities) {
+        if (playerId.player.summonerId === summId) {
+            var summMatchId = playerId.participantId;
+            var summWon = (summMatchId <= 5) ? (match.teams[0].win === "Win") : (match.teams[1].win === "Win");
+        }
+    }
+    var teamBase = summMatchId <= 5 ? 1 : 6;
+    var teamMatchIds;
+    for (var i = teamBase; i <= teamBase + 4; i++) {
+        if (i !== summonerId)
+            teamMatchIds.push(i);
+    }
+    var persPerf = performance([summId]);
+    var teamPerf = performance(teamMatchIds);
+    return summWon ? (persPerf - teamPerf) : (teamPerf - persPerf);
+}
+
+function performance(summIds) {
+    var totalPerf = 0;
+    for (var id of summIds) {
+        totalPerf += eachPerf(id);
+    }
+    return totalPerf/summIds.length;
+}
+
+function eachPerf(summId) {
+    return summId;
 }
 
 function winrate(winsList) {
@@ -36,10 +71,10 @@ function parseMatches(matchList, summId) {
     return matchList.map(match => parseMatchWin(match, summId));
 }
 
-function parseMatchWin(match, summId) {
+/* function parseMatchWin(match, summId) {
     for (var player of match.participants) {
         if (player.participantId === summId)
             return player.stats.win
     }
     return false; // should never happen
-}
+} */
