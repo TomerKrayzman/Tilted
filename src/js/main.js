061b5f5bc882e607eaed450635a9a13518610353
Braeden Mollot <3mollot@gmail.com>
2018-04-08 00:56:10 -0400

    Merge

--- src/js/main.js
+++ src/js/main.js
@@ -12,11 +12,11 @@ document.querySelector('#sname-but').addEventListener('click', () => {
   // TODO: Input sanitization
   const xhr = new XMLHttpRequest()
   xhr.open('GET', summonerByNameUri(sname), false) // syncronous request
-  xhr.setRequestHeader("Origin", null)
-  xhr.setRequestHeader("Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8")
+  // xhr.setRequestHeader("Origin", null)
+  // xhr.setRequestHeader("Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8")
   // xhr.setRequestHeader("X-Riot-Token", "RGAPI-09105804-5cbb-49dc-9071-83d9d7c15779")
-  xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5")
-  xhr.setRequestHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0")
+  // xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5")
+  // xhr.setRequestHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0")
   xhr.send()
   
   // TODO: Error/sanity checks
